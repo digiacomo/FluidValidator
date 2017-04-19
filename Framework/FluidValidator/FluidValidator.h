d4a164d45245d2eac7967295f87890ae80e93246
FrogRain <info@frograin.com>
2017-04-19 20:03:13 +0100

    added support for Carthage

---
+++ Framework/FluidValidator/FluidValidator.h
@@ -0,0 +1,19 @@
+//
+//  FluidValidator.h
+//  FluidValidator
+//
+//  Created by Antonio Di Giacomo on 19/04/2017.
+//  Copyright © 2017 Frograin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FluidValidator.
+FOUNDATION_EXPORT double FluidValidatorVersionNumber;
+
+//! Project version string for FluidValidator.
+FOUNDATION_EXPORT const unsigned char FluidValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FluidValidator/PublicHeader.h>
+
+
