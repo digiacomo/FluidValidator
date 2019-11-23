fa6586f565476abfa58ca2940bb152ae2295a3ac
Antonio Di Giacomo <digiacomo.antonio@gmail.com>
2019-11-23 19:45:43 +0000

    Initial Commit

---
+++ Valid/Valid.h
@@ -0,0 +1,19 @@
+//
+//  Valid.h
+//  Valid
+//
+//  Created by Antonio Di Giacomo on 23/11/2019.
+//  Copyright © 2019 Antonio Di Giacomo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Valid.
+FOUNDATION_EXPORT double ValidVersionNumber;
+
+//! Project version string for Valid.
+FOUNDATION_EXPORT const unsigned char ValidVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Valid/PublicHeader.h>
+
+
---
+++ ValidTests/ValidTests.swift
@@ -0,0 +1,34 @@
+//
+//  ValidTests.swift
+//  ValidTests
+//
+//  Created by Antonio Di Giacomo on 23/11/2019.
+//  Copyright © 2019 Antonio Di Giacomo. All rights reserved.
+//
+
+import XCTest
+@testable import Valid
+
+class ValidTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
